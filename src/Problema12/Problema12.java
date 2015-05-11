1348dc069e0ad8e65914342d1ad68d60751067d6
Carlos <Carlos@148.229.22.47>
2015-05-11 16:59:05 -0600

    Agrego problemas hasta el numero 12

--- src/Problema12/Problema12.java
+++ src/Problema12/Problema12.java
@@ -10,5 +10,21 @@ package Problema12;
  * @author Carlos
  */
 public class Problema12 {
+    public static void main(String[] args) {
+        int SumaPares = 0, SumaImpares = 0;
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+            if (i%2==0) {
+                SumaPares += i;                
+            }
+            else{
+                SumaImpares += i;
+            }
+            
+        }
+        System.out.println("Suma de numeros pares calculados por un lado = jaja"
+                    + " "+SumaPares+" Suma de impares calculados por el otro "
+                    + "lado = "+SumaImpares);
+    }
     
 }
